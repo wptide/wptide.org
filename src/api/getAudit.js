447d2352e42979831d910eca4166f09517cef1ed
Ivan@Kruchkoff.com <ivan@kruchkoff.com>
2020-10-26 14:56:09 -0700

    Invariant check on getAudit.

--- src/api/getAudit.js
+++ src/api/getAudit.js
@@ -1,3 +1,5 @@
+const invariant = require('invariant');
+
 /**
  * Gets an existing Audit
  *
@@ -5,6 +7,10 @@
  * @param res
  */
 const getAudit = (req, res) => {
-    res.json( { action: 'getAudit', params: req.params } );
-}
+    invariant(req.params.project_type, 'Project type missing');
+    invariant(['theme', 'plugin'].includes(req.params.project_type), 'Project type should be theme or plugin');
+    invariant(req.params.project_slug, 'Project slug missing');
+    invariant(req.params.version, 'Version missing');
+    res.json({ action: 'getAudit', params: req.params });
+};
 module.exports = getAudit;
