98f21d9c2b9cfb99e9598fa4c71054422f3104cd
Derek Herman <derek.herman@xwp.co>
2020-12-11 16:18:15 -0800

    Set the ack deadline to 300 locally like it is on GCP

--- app/src/run/proxyServer.js
+++ app/src/run/proxyServer.js
@@ -6,9 +6,11 @@ const { messageTypes, subscribe } = require('../integrations/pubsub');
 const main = async () => {
     await subscribe(messageTypes.MESSAGE_TYPE_PHPCS_REQUEST, {
         pushEndpoint: 'http://localhost:8110',
+        ackDeadlineSeconds: 300,
     });
     await subscribe(messageTypes.MESSAGE_TYPE_LIGHTHOUSE_REQUEST, {
         pushEndpoint: 'http://localhost:8090',
+        ackDeadlineSeconds: 300,
     });
 };
 
