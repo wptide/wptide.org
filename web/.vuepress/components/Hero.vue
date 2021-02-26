95070dee4d0aec175e60f526ad9ac9fc51d7694f
Derek Herman <derek.herman@xwp.co>
2021-02-25 23:13:53 -0800

    Fix Hero height

--- web/.vuepress/components/Hero.vue
+++ web/.vuepress/components/Hero.vue
@@ -62,7 +62,7 @@ export default {
 <style lang="stylus">
 .hero
   position relative
-  height calc(100vh -13.1rem)
+  height calc(100vh -16.6rem)
 
 .hero__brand
   display flex
@@ -114,7 +114,7 @@ export default {
 @media (min-width: 960px)
   .hero
     padding-top 10vh
-    height calc(90vh -13.1rem)
+    height calc(90vh -16.6rem)
 
   .hero__canvas
     display block
