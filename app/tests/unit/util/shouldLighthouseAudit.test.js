ac3d13e1ac3f2744e981ce9f339b8561cac7aed6
Derek Herman <derek.herman@xwp.co>
2021-07-26 15:28:01 -0700

    Update latest twentytwenty tested theme version

--- app/tests/unit/util/shouldLighthouseAudit.test.js
+++ app/tests/unit/util/shouldLighthouseAudit.test.js
@@ -7,8 +7,8 @@ const { shouldLighthouseAudit } = require('../../../src/util/shouldLighthouseAud
  * Tests for shouldLighthouseAudit.
  */
 describe('shouldLighthouseAudit', () => {
-    it('Returns true for the TwentyTwenty theme v1.7', async () => {
-        expect(await shouldLighthouseAudit({ slug: 'twentytwenty', version: '1.7' })).toBe(true);
+    it('Returns true for the TwentyTwenty theme v1.8', async () => {
+        expect(await shouldLighthouseAudit({ slug: 'twentytwenty', version: '1.8' })).toBe(true);
     });
     it('Returns false for the TwentyTwenty theme v1.5', async () => {
         expect(await shouldLighthouseAudit({ slug: 'twentytwenty', version: '1.5' })).toBe(false);
