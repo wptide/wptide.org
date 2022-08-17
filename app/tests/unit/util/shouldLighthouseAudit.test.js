671785f9a88eba4dc424d3710dc374e807c79059
Bartosz Gadomski <kontakt@bartoszgadomski.pl>
2022-08-17 15:07:58 +0200

    fix failing tests

--- app/tests/unit/util/shouldLighthouseAudit.test.js
+++ app/tests/unit/util/shouldLighthouseAudit.test.js
@@ -7,8 +7,8 @@ const { shouldLighthouseAudit } = require('../../../src/util/shouldLighthouseAud
  * Tests for shouldLighthouseAudit.
  */
 describe('shouldLighthouseAudit', () => {
-    it('Returns true for the TwentyTwenty theme v1.8', async () => {
-        expect(await shouldLighthouseAudit({ slug: 'twentytwenty', version: '1.8' })).toBe(true);
+    it('Returns true for the TwentyTwenty theme v2.0', async () => {
+        expect(await shouldLighthouseAudit({ slug: 'twentytwenty', version: '2.0' })).toBe(true);
     });
     it('Returns false for the TwentyTwenty theme v1.5', async () => {
         expect(await shouldLighthouseAudit({ slug: 'twentytwenty', version: '1.5' })).toBe(false);
